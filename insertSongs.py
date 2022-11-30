a5c3be92394c04d1803fd3df92e4ccae2a2ef995
therealmarius <70507259+therealmarius@users.noreply.github.com>
2022-11-30 21:44:22 +0100

    Update insertSongs.py

--- insertSongs.py
+++ insertSongs.py
@@ -11,12 +11,12 @@ with open('itunes.csv') as itunes_identifiers_file:
     for line in itunes_identifiers_file:
         itunes_identifier = int(line)
         print(f"\nAdding song with iTunes identifier {itunes_identifier}...")
-        pyautogui.click(899, 768)
+        pyautogui.click(<REMPLACE THIS>, <REMPLACE THIS>)
         pyautogui.hotkey("ctrl", "a")
         pyautogui.hotkey("ctrl", "v")
-        pyautogui.click(899, 768)
+        pyautogui.click(<REMPLACE THIS>, <REMPLACE THIS>)
         pyautogui.typewrite(str(itunes_identifier))
-        pyautogui.click(1180, 1015)
+        pyautogui.click(<REMPLACE THIS>, <REMPLACE THIS>)
         time.sleep(2)
         print("Song added!")
 
